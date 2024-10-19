dae29fa497dce1ea470ae84883cec4556370133b
ahd2 <2321308284@qq.com>
2024-10-19 21:47:40 +0800

    Added 顶点缓冲区01

--- AHD2-LearnOpenGL/src/Main.cpp
+++ AHD2-LearnOpenGL/src/Main.cpp
@@ -1,5 +1,6 @@
-#include <GLFW/glfw3.h>
-
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+#include <iostream>
 int main(void)
 {
     GLFWwindow* window;
@@ -18,17 +19,35 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    //初始化glew
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "error!" << std::endl;
+        return -1;
+    }
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    float vertices[] = {
+    0.0f, 1.0f,
+    1.0f, 0.0f,
+    -1.0f, -1.0f
+    };
+
+    unsigned int buffer;
+    //生成一个缓冲区
+    glGenBuffers(1, &buffer);
+    //指定为顶点缓冲区
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    //填充当前绑定的缓冲区（即上面的顶点缓冲区）
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), vertices, GL_STATIC_DRAW);
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        glBegin(GL_TRIANGLES);
-        glVertex2f(0.0f, 1.0f);
-        glVertex2f(1.0f, 0.0f);
-        glVertex2f(-1.0f, -1.0f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
